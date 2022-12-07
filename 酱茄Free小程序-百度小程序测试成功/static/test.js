306ef0436721083aa486a5817ac38299c413de1a
wwy <39434325+wangwei39120157028@users.noreply.github.com>
2022-12-07 22:42:26 +0800

    4th_3
    第四次提交代码文件

---
+++ 酱茄Free小程序-百度小程序测试成功/static/test.js
@@ -0,0 +1,8 @@
+
+function test(){
+  document.write("<script language=javascript src='/static/report_sdk.js'></script>");
+  console.log("test---------------------")
+}
+module.exports = {
+  test
+}
